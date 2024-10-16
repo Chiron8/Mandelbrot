bf2fcba9d885d7bee8fceef6f465c604f96b9790
Ch1llyB1lly <135429057+Chiron8@users.noreply.github.com>
2024-10-16 17:12:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import time
+start = time.time()
+img = Image.effect_mandelbrot((50000, 50000), (0.33, 0.35, 0.38, 0.4), 200)
+img = img.save("output1.jpg")
+end = time.time()
+print(end-start)
