e6b91efead71d5e5c9ebab9b7cc83bcc28a2ae60
Ch1llyB1lly <135429057+Chiron8@users.noreply.github.com>
2024-10-17 07:10:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import time
 start = time.time()
-img = Image.effect_mandelbrot((50000, 50000), (0.33, 0.35, 0.38, 0.4), 200)
+#img = Image.effect_mandelbrot((50000, 50000), (0.33, 0.35, 0.38, 0.4), 200)
+img = Image.effect_mandelbrot((50000, 50000), (-2, -2, 2, 2), 200)
 img = img.save("output1.jpg")
 end = time.time()
 print(end-start)
